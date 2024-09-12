75df66a5aa1c38865141916abdb168fb31ee3dbf
ajuxtapose22 <aaron.jardin@yahoo.com>
2024-09-12 16:23:39 -0700

    GET request implemented and tested

--- api/server.js
+++ api/server.js
@@ -1,3 +1,64 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
+const User = require('./users/model')
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express()
+const port = 9000
+
+// GET Users 
+server.get('/api/users', async (req, res) => {
+    try {
+        const user = await User.find()
+        res.status(200).json({
+            message: "Success GET of a User!",
+            data: user,
+        })
+    } catch(error) {
+        res.status(500).json({
+            message: "The users information could not be retrieved",
+            error: error.message,
+    })
+    }
+})
+
+
+// GET 	/api/users/:id
+server.get('/api/users/:id', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+        if (!user) {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist"})
+        } else {
+            res.status(200).json({
+            message: "Success GET of a User!",
+            data: user,
+            })
+        }
+    } catch (error) {
+            res.status(500).json({
+            message: "The user information could not be retrieved",
+            error: error.message,
+        })
+    }           
+})
+
+// POST /api/users 
+server.post('/api/users', async (req, res) => {
+    
+})
+
+// PUT /api/users/:id
+
+// DELETE 	/api/users/:id
+
+
+
+
+
+//Listen to Port
+server.listen(port, () => {
+    console.log("Server running on http://localhost:9000")
+})
+
+module.exports = server // EXPORT YOUR SERVER instead of {}
